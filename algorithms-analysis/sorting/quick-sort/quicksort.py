5b6576105d449eecf6b62ec1c5be3078319dea3c
rahulbali <rahulrdb18@gmail.com>
2022-05-22 06:39:23 +0000

    Update algorithms-analysis/sorting/quick-sort/quicksort.py

--- algorithms-analysis/sorting/quick-sort/quicksort.py
+++ algorithms-analysis/sorting/quick-sort/quicksort.py
@@ -2,6 +2,18 @@
 
 """
 
+Input:
+7 4 8 9 2 5 1 6
+
+
+
+
+Quick Sort algo:
+1. Choose a pivot, partition the array at the pivot element. Generally, the middle element.
+2. Start 2 pointers from start and end.
+3. Swap the elements if the start is bigger than the end, and the end element is smaller than pivot.
+4. Move one position, after swap, then check point 3 again.
+5. Once completed, start from 1 to 4 in the left, then right partition. Until sorted. :)
 
 
 """
