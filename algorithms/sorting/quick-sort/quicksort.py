9775d238902d6ab8c032db92e5a094d2d35a5a52
rahulbali <rahulrdb18@gmail.com>
2022-05-22 17:54:01 +0000

    Update algorithms/sorting/quick-sort/quicksort.py

--- algorithms/sorting/quick-sort/quicksort.py
+++ algorithms/sorting/quick-sort/quicksort.py
@@ -14,5 +14,51 @@ Quick Sort algo:
 5. Once completed, start from 1 to 4 in the left, then right partition. Until sorted. :)
 
 
+Method design:
+
+- partition(A, start, end)
+this function will take input of the Array, start, end position.
+And choose pivot element and will return the two partitions.
+
+- quicksort(A, x, y)
+this function will be used for recursion of the algo.
+function is called with smaller set of start and end, for each iteration.
+And will perform swap of elements in place.
+
 """
+
+def partition(A, start, end):
+    """
+    create partition from the 
+    """
+    mid = ( start + end ) / 2
+    return mid
+
+
+def quicksort(A, first, last):
+    pivot = partition(A, start, end)
+    # partition 1
+    start = first
+    end = last
+    while start == pivot and end == pivot:
+        if A[start] > A[end]:
+            A[start], A[end] = A[end], A[start]
+            start = start + 1
+            end = end - 1
+        elif start != pivot:
+            end = end - 1
+        elif end != pivot:
+            start = start + 1
+    quicksort(A, first, pivot-1)
+    quicksort(A, pivot + 1, last)
+
+
+
+
+
+
+
+
+
+
 
