834cd71aef847a77a5a3b6308fcb4b009c533e48
rahbal <rahulrdb18@gmail.com>
2022-05-31 14:55:34 +0530

    fixed sequence of altering LL nodes while inserting

--- data-structures/linked-lists/circular-linked-list.py
+++ data-structures/linked-lists/circular-linked-list.py
@@ -1,10 +1,11 @@
 ## we know what circular linked list is, now we implement in python.
 
 
-class Node(value):
-    self.value = value
-    self.prev = None
-    self.nxt = None
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.nxt = None
 
 class CircularLinkedList:
     """CircularLinkedList.
@@ -15,10 +16,9 @@ class CircularLinkedList:
     Delete
 
     """
-    self.head = None
 
-    def __init__(self, value):
-        self.insert(value)
+    def __init__(self):
+        self.head = None
 
     def insert(self, value):
         if self.head is None:
@@ -26,26 +26,39 @@ class CircularLinkedList:
         else:
             new_node = Node(value)
 
-            # linking new nonde to head
-            self.head.prev = new_node
-            new_node.nxt = self.head
-
+            """ if there is exists a preivous node to head,
+            we take that last_node, add new_node to next of last.
+            and add last_node to the previous of new_node.
+            """
             if self.head.prev is not None:
                 # prev node exists
                 last_node = self.head.prev
                 last_node.nxt = new_node
                 new_node.prev = last_node
 
+            """ if there is no next node to head,
+            we add new_node as next node to head.
+            """
+            if self.head.nxt is None:
+                self.head.nxt = new_node
+                new_node.prev = self.head
+
+            # linking new node to head, will be done in all cases.
+            self.head.prev = new_node
+            new_node.nxt = self.head
+
     def traverse(self):
         current = self.head
-        while current.nxt:
-            print(str(current.value), ' ')
+        print(current.value)
+        while current.nxt != self.head:
+            print(str(current.nxt.value), ' ')
             current = current.nxt
 
 
 if __name__ == "__main__":
-    c = CircularLinkedList(4)
+    c = CircularLinkedList()
     c.insert(9)
+    print('comes here')
     c.insert(7)
     c.insert(1)
     c.insert(0)
